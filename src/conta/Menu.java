a0980d369bffb537f7dc334810701849c4ca8e2d
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-06-17 10:05:47 -0300

    Task 4 resolvida - exception

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,6 +1,8 @@
 package conta;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.uil.Cores;
@@ -9,7 +11,7 @@ public class Menu {
 
 	public static void main(String[] args) {
 		// Teste da Classe Conta
-		
+
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
 		cc1.visualizar();
@@ -17,23 +19,22 @@ public class Menu {
 		cc1.visualizar();
 		cc1.depositar(5000.0f);
 		cc1.visualizar();
-		
-        // Teste da Classe Conta Poupança
+
+		// Teste da Classe Conta Poupança
 		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
 		cp1.visualizar();
-        cp1.sacar(1000.0f);
+		cp1.sacar(1000.0f);
 		cp1.visualizar();
 		cp1.depositar(5000.0f);
 		cp1.visualizar();
-		
-		
+
 		// import Scanner
 		Scanner sc = new Scanner(System.in);
 
 		// declrarando variaveis
 		int opc;
 
-		while(true) {
+		while (true) {
 
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
 					+ "*****************************************************");
@@ -55,62 +56,68 @@ public class Menu {
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
-			
-			opc = sc.nextInt();
-				
+
+			try {
+				opc = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				sc.nextLine();
+				opc = 0;
+			}
+
 			if (opc == 9) {
-				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
 				sobre();
 				sc.close();
 				System.exit(0);
 			}
-				
-			switch (opc) {
-				case 1:
-					System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
-				
-                    		break;
-				case 2:
-					System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
-					
-                    		break;
-				case 3:
-					System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
-	
-                    		break;
-				case 4:
-					System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
-					
-                    		break;
-				case 5:
-					System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
-		
-                   	 	break;
-				case 6:
-					System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
-                   	 	break;
-				case 7:
-					System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
-					
-                    		break;
-				case 8:
-					System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
-					
-                    		break;
-				default:
-					System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
-                    		break;
+			switch (opc) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
+
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
+
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+
+				break;
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				break;
 			}
 		}
-    }
+	}
 
-    public static void sobre() {
-	System.out.println("\n*********************************************************");
-	System.out.println("Projeto Desenvolvido por: ");
-	System.out.println("Generation Brasil - generation@generation.org");
-	System.out.println("github.com/conteudoGeneration");
-	System.out.println("*********************************************************");
-   }
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Generation Brasil - generation@generation.org");
+		System.out.println("github.com/conteudoGeneration");
+		System.out.println("*********************************************************");
+	}
 
 }
